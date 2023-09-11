343633943bf0d5e44fd568e37b3a573e89664015
winters-ag <winters.alan.g@gmail.com>
2023-09-10 22:45:56 -0500

    created templates for pages, included google font, updated navbar with links

--- navBar.js
+++ navBar.js
@@ -1,7 +1,11 @@
+let navBar = document.createElement('script');
+navBar.src = 'navBar.html';
+
+
 const headerTemplate = document.createElement('template');
     headerTemplate.innerHTML = `
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="./landing.html">Alan Winters</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -10,24 +14,21 @@ const headerTemplate = document.createElement('template');
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="./landing.html">Bio<span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown
+          <a class="nav-link dropdown-toggle" href="./projects.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Projects
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="./projects.html">Overview</a>
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
+            <a class="dropdown-item" href="./bustracking.html">Bus Tracking</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="./pacmen.html">PacMen</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="./eyemovement.html">Eye Tracking</a>
           </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
         </li>
       </ul>
       <form class="form-inline my-2 my-lg-0">
