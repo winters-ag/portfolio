ef7977ae8df49ecf71b38bc82eddc5cdec83d84f
winters-ag <winters.alan.g@gmail.com>
2023-09-23 12:58:37 -0500

    too many changes for one commit

--- navBar.js
+++ navBar.js
@@ -5,39 +5,37 @@ navBar.src = 'navBar.html';
 const headerTemplate = document.createElement('template');
     headerTemplate.innerHTML = `
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="./index.html">Alan Winters</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary rounded navbar-dark bg-dark" aria-label="Thirteenth navbar example">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample11" aria-controls="navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
   
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="./index.html">Bio<span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="./projects.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Projects
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" target="_blank" href="./projects.html">Overview</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" target="_blank" href="https://winters-ag.github.io/BusTracking/">Bus Tracking</a>
-            <a class="dropdown-item" target="_blank" href="https://github.com/winters-ag/BusTracking">Bus Tracking Repo</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" target="_blank" href="https://winters-ag.github.io/PacMen_Exercise/">PacMen</a>
-            <a class="dropdown-item" target="_blank" href="https://github.com/winters-ag/PacMen_Exercise">PacMen Repo</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" target="_blank" href="https://winters-ag.github.io/Eye_Tracking/">Eye Tracking</a>
-            <a class="dropdown-item" target="_blank" href="https://github.com/winters-ag/Eye_Tracking">Eye Tracking Repo</a>
-          </div>
-        </li>
-      </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+      <div class="collapse navbar-collapse d-lg-flex" id="navbarsExample11">
+        <a class="navbar-brand col-lg-3 me-0" href="./index.html">Alan Winters</a>
+        <ul class="navbar-nav col-lg-6 justify-content-end">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="./index.html">Bio</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="./projects.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Projects
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" target="_blank" href="./projects.html">Overview</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" target="_blank" href="https://winters-ag.github.io/BusTracking/">Bus Tracking</a>
+              <a class="dropdown-item" target="_blank" href="https://github.com/winters-ag/BusTracking">Bus Tracking Repo</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" target="_blank" href="https://winters-ag.github.io/PacMen_Exercise/">PacMen</a>
+              <a class="dropdown-item" target="_blank" href="https://github.com/winters-ag/PacMen_Exercise">PacMen Repo</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" target="_blank" href="https://winters-ag.github.io/Eye_Tracking/">Eye Tracking</a>
+              <a class="dropdown-item" target="_blank" href="https://github.com/winters-ag/Eye_Tracking">Eye Tracking Repo</a>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
     `;
